8bcb85428c089b9e2389eeec7e77ae4b484ea671
ariaramin <ariaramin24@gmail.com>
2021-02-15 14:04:13 +0330

    updated

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from category.models import Category
+
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', ]
+    list_filter = ['created_at', ]
+    search_fields = ['title', 'description']
+
+
+admin.site.register(Category, CategoryAdmin)
